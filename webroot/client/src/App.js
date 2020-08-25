f3835eb4f92d6498cf87796d252801f284eb6552
Sven Zetterlund <private@email.com>
2020-08-25 17:33:12 -0500

    Install 'prettier' node package for formatting JavaScript

--- webroot/client/src/App.js
+++ webroot/client/src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-
-//Write your javascript here, or roll your own. It's up to you.
-//Make your ajax call to http://localhost:8765/api/index.php here
-
+  //Write your javascript here, or roll your own. It's up to you.
+  //Make your ajax call to http://localhost:8765/api/index.php here
 
   return (
     <div className="App">
-        <p>Country Search within React</p>
+      <p>Country Search within React</p>
     </div>
   );
 }
